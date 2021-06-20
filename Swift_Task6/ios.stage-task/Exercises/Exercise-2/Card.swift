102f5c36acfc92890c282551ca0203acf682a5dc
Sergey <80987789+SergeyGordeychik86@users.noreply.github.com>
2021-06-20 19:12:34 +0300

    Update Card.swift

--- Swift_Task6/ios.stage-task/Exercises/Exercise-2/Card.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-2/Card.swift
@@ -15,7 +15,7 @@ enum Suit: Int, CaseIterable, Codable {
     case diamonds
 }
 
-enum Value: Int, Codable {
+enum Value: Int, CaseIterable, Codable {
     case six
     case seven
     case eight
@@ -33,10 +33,16 @@ struct Card: CardBaseCompatible {
     var isTrump: Bool = false
 
     func hash(into hasher: inout Hasher) {
+        hasher.combine(suit)
+        hasher.combine(value)
 
     }
 
     static func == (lhs: Self, rhs: Self) -> Bool {
+        
+         if lhs.suit == rhs.suit && lhs.value == rhs.value {
+            return true
+        }
         return false
     }
 }
@@ -44,10 +50,27 @@ struct Card: CardBaseCompatible {
 extension Card {
 
     func checkIfCanBeat(card: Card) -> Bool {
+        
+        if self.suit == card.suit && checkValue(card: card) {
+            return true
+        }
+        if self.isTrump && card.isTrump && checkValue(card: card) {
+            return true
+        }
+        if self.isTrump && !card.isTrump {
+            return true
+        }
         return false
     }
 
     func checkValue(card: Card) -> Bool {
+        let values = Value.allCases
+        let indexCard = values.firstIndex(where: {$0 == card.value})!
+        let indexCardSelf = values.firstIndex(where: {$0 == self.value})!
+        
+        if indexCardSelf > indexCard {
+            return true
+        }
         return false
     }
 }
