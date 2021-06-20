7039f218f9cb80c3953a4c63352590bd6b3b1d1f
Sergey <80987789+SergeyGordeychik86@users.noreply.github.com>
2021-06-20 19:17:59 +0300

    Update Game.swift

--- Swift_Task6/ios.stage-task/Exercises/Exercise-2/Game.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-2/Game.swift
@@ -17,7 +17,21 @@ struct Game: GameCompatible {
 
 extension Game {
 
-    func defineFirstAttackingPlayer(players: [Player]) -> Player? {
-        nil
+     func defineFirstAttackingPlayer(players: [Player]) -> Player? {
+        var firtsPlayer = Player()
+        let values = Value.allCases
+        var tmp = 8
+        for player in players {
+            for index in  0 ..< 6 {
+                if ((player.hand?[index].isTrump) == true) {
+                    let indexCard = values.firstIndex(where: {$0 == player.hand?[index].value})!
+                    if  indexCard <= tmp {
+                        tmp = indexCard
+                        firtsPlayer = player
+                    }
+                }
+            }
+        }
+        return firtsPlayer
     }
 }
